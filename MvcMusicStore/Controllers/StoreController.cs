b4e3c65daa9293d31d27ff0164e7d47a25a4ceab
D Vijay <D Vijay@DESKTOP-93TNJMI>
2017-03-08 15:39:02 +0530

    Add project files.

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Browse(string genre)
+        {
+            string msg = HttpUtility.HtmlEncode("Store.Browse , Genre = " + genre);
+            return msg;
+        }
+
+        public string Details(int id)
+        {
+            return "Store.Details, ID = " + id;
+        }
+    }
+}
